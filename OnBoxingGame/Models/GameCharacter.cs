089b9b48911187b5a62cf7b95fc227eb4a77611e
Abdulrahman Elheyb <45883300+Abdulrahmanelheyb@users.noreply.github.com>
2023-08-20 12:57:43 +0300

    Create GameCharacter.cs

---
+++ OnBoxingGame/Models/GameCharacter.cs
@@ -0,0 +1,9 @@
+﻿using OnBoxingGame.Common;
+
+namespace OnBoxingGame.Models;
+
+public class GameCharacter
+{
+    public string Name { get; set; }
+    public Constants.RarityLevel Rarity { get; set; }
+}
