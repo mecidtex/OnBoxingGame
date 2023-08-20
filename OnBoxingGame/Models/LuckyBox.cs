606de0d30117950aad58049ce07a55020f657114
Abdulrahman Elheyb <45883300+Abdulrahmanelheyb@users.noreply.github.com>
2023-08-20 12:57:46 +0300

    Create LuckyBox.cs

---
+++ OnBoxingGame/Models/LuckyBox.cs
@@ -0,0 +1,14 @@
+﻿using OnBoxingGame.Common;
+
+namespace OnBoxingGame.Models;
+
+public class LuckyBox
+{
+    // Gives bonus money
+    public int Bonus { get; set; }
+    
+    // Gives XP to score
+    public int Xp { get; set; }
+
+    public GameCharacter Character { get; set; }
+}
